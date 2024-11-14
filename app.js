afc7d45435926c05773fa2d628eae5c7199294f3
Reihan Agam <reihanagam7@gmail.com>
2024-11-14 17:10:16 +0700

    update

--- app.js
+++ app.js
@@ -87,14 +87,14 @@ async function handleSos(ws, message) {
     const agent = clients.get("0f9815b3-01a2-4350-8679-2e9b8b1637b7")
 
     const sender = await User.getProfile(user_id)
-    const receiver = await User.getUser("0f9815b3-01a2-4350-8679-2e9b8b1637b7")
+    // const receiver = await User.getUser("0f9815b3-01a2-4350-8679-2e9b8b1637b7")
 
-    const kbri = await Kbri.userKbri("0f9815b3-01a2-4350-8679-2e9b8b1637b7")
+    // const kbri = await Kbri.userKbri("0f9815b3-01a2-4350-8679-2e9b8b1637b7")
 
-    var agentId = receiver.length == 0 ? "-" : receiver[0].user_id
-    var agentName = receiver.length == 0 ? "-" : receiver[0].username
+    // var agentId = receiver.length == 0 ? "-" : receiver[0].user_id
+    // var agentName = receiver.length == 0 ? "-" : receiver[0].username
 
-    var continent = kbri.length == 0 ? "-" : kbri[0].continent_name
+    // var continent = kbri.length == 0 ? "-" : kbri[0].continent_name
 
     var sosId = uuidv4()
 
@@ -132,9 +132,6 @@ async function handleSos(ws, message) {
             time: time,
             lat: lat, 
             lng: lng,
-            continent: continent,
-            agent_id: agentId,
-            agent_name: agentName,
             is_confirm: false,
         }))
 
