43290756b16e25c08fe2c0066407c33b2fdad0a7
Reihan Agam <reihanagam7@gmail.com>
2024-12-04 16:43:11 +0700

    update

--- models/Chat.js
+++ models/Chat.js
@@ -344,11 +344,9 @@ module.exports = {
     insertMessage: (msgId, chatId, sender, recipient, content) => {
         return new Promise((resolve, reject) => {
             const query = `INSERT INTO messages (uid, chat_id, sender_id, receiver_id, content, ack, type)
-            VALUES (?, ?, ?, ?, ?, ?, ?)`
+            VALUES (?, ?, ?, ?, ?, 1, 1)`
             
-            const values = [msgId, chatId, sender, recipient, content, 1, 1]
-
-            console.log(values)
+            const values = [msgId, chatId, sender, recipient, content]
 
             conn.query(query, values, (e, result) => {
                 if(e) {
